8d567ecd77eea85f34c79086c694a062f70a8ef0
Kevin Kuchta <kevin@kevinkuchta.com>
2014-02-22 15:06:57 -0800

    Initial revision (clean rails)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vimbits2::Application.config.secret_key_base = '1844323b2e7af9174ad67a8e8f06a608e716f01c8f2cfdd4e6167ff01ed9577605b90ba510e6e75fbf2f9762d80ce83ac072b07fa22ac3193efb1a417cd26477'
