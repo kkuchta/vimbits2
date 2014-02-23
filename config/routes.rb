5afd3e85af1948b313ea57254929c8c53d78b498
Kevin Kuchta <kevin@kevinkuchta.com>
2014-02-22 16:45:48 -0800

    Add basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Vimbits2::Application.routes.draw do
+  #get "sessions/new"
+  #get "sessions/create"
+  #get "sessions/destroy"
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'sessions#new', :as => :login
+  post 'logout' => 'sessions#destroy', :as => :logout
+
   resources :users
 
   resources :bits
@@ -7,7 +15,7 @@ Vimbits2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
